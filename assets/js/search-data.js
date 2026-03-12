00253700936372f285b79616a28156f420a92da5
5gyumin819999 <5gyumin819999@gmail.com>
2026-03-12 02:53:20 +0000

    Deploying to gh-pages from @ jsbyun3/IN-SB-Lab@6ba87d7b449681ef18c8c4702c6ec31ab6ad7517 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,19 +30,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/IN-SB-Lab/publications/";
           },
-        },{id: "nav-software",
-          title: "software",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/IN-SB-Lab/software/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Course materials, schedules, and resources for classes taught.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/IN-SB-Lab/teaching/";
+          },
+        },{id: "nav-software",
+          title: "software",
+          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/IN-SB-Lab/software/";
           },
         },{id: "nav-news",
           title: "news",
